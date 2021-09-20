d4b10822282a9863bcebd807b9e5c60f4649372e
Anton Nikishkin <nikishkin.anton@gmail.com>
2021-09-21 00:50:18 +0300

    Task 3: Add simple functionality

---
+++ Module01/ActorsRazorPages/Pages/Actors/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+using ActorsRazorPages.Models;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ActorsRazorPages.Pages.Actors
+{
+    public class IndexModel : PageModel
+    {
+        private IData _data;
+
+        public IndexModel(IData data)
+        {
+            _data = data;
+        }
+
+        public IList<Actor> Actors { get; set; }
+
+        public void OnGet()
+        {
+            Actors = _data.ActorsInitializeData();
+        }
+    }
+}
--- Module01/ActorsRazorPages/Startup.cs
+++ Module01/ActorsRazorPages/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using ActorsRazorPages.Models;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +25,8 @@ namespace ActorsRazorPages
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IData, Data>();
+
             services.AddRazorPages();
         }
 
