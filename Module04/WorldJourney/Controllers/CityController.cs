b90a78f0ba660d53a411fd44cb779007252deb63
Anton Nikishkin <nikishkin.anton@gmail.com>
2021-09-21 16:44:45 +0300

    Task 1: Add controllers to an MVC application

---
+++ Module04/WorldJourney/Controllers/CityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorldJourney.Controllers
+{
+    public class CityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
