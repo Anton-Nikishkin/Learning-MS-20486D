1cae3caf114c8c1b16e942c721f7288f69a1e3a2
Anton Nikishkin <nikishkin.anton@gmail.com>
2021-09-21 16:54:48 +0300

    Task 2: Add actions to a controller

--- Module04/WorldJourney/Controllers/CityController.cs
+++ Module04/WorldJourney/Controllers/CityController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IO;
+
+using Microsoft.AspNetCore.Mvc;
+
+using WorldJourney.Models;
 
 namespace WorldJourney.Controllers
 {
@@ -6,7 +10,43 @@ namespace WorldJourney.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["Page"] = "Search city";
+
             return View();
+        }
+
+        public IActionResult Details()
+        {
+            ViewData["Page"] = "Search city";
+
+            City city = null;
+
+            if (city == null) return NotFound();
+
+            return View(city);
+        }
+
+        public IActionResult GetImage()
+        {
+            ViewData["Message"] = "Display Image";
+
+            City requestedCity = null;
+            if (requestedCity != null)
+            {
+                var fullPath = "";
+                var fileOnDisk = new FileStream(fullPath, FileMode.Open);
+                byte[] fileBytes;
+                using (BinaryReader br = new BinaryReader(fileOnDisk))
+                {
+                    fileBytes = br.ReadBytes((int)fileOnDisk.Length);
+                }
+                return File(fileBytes, requestedCity.ImageMimeType);
+
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
