f640b256cfc24c70db5d02ef86d6af75115e1d81
Anton Nikishkin <nikishkin.anton@gmail.com>
2021-09-23 14:29:10 +0300

    Exercise 1: Adding Entity Framework Core - Task 1: Create model classes

---
+++ Module07/Cupcakes/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cupcakes
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(routes =>
+            {
+                routes.MapControllerRoute(
+                    name: "CupcakeRoute",
+                    pattern: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Cupcake", action = "Index" },
+                    constraints: new { id = "[0-9]+" });
+            });
+        }
+    }
+}
