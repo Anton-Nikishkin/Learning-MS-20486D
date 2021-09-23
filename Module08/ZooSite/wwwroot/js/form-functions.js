4eb1b1ed6f30345a537f7f628cb649e6c12eefbf
Anton Nikishkin <nikishkin.anton@gmail.com>
2021-09-24 00:28:03 +0300

    Exercise 4: Using jQuery - Task 2: Use jQuery to add event handlers

--- Module08/ZooSite/wwwroot/js/form-functions.js
+++ Module08/ZooSite/wwwroot/js/form-functions.js
@@ -1,10 +1,22 @@
-﻿function calculateSum() {
-    var rows = document.querySelectorAll("#totalAmount tr .sum");
-    var sum = 0;
+﻿$(function () {
+    $('.pricing select').change(function (event) {
+        var target = $(event.target);
+        var value = parseInt(target.val());
+        var container = target.parent();
+        var price = container.prev();
+        var label = price.prev();
 
-    for (var i = 0; i < rows.length; i++) {
-        sum = sum + parseFloat(parseFloat(rows[i].innerHTML.substring(1, rows[i].innerHTML.length)).toFixed(2));
+        $("#" + label.text()).remove();
+    });
+
+    function calculateSum() {
+        var rows = document.querySelectorAll("#totalAmount tr .sum");
+        var sum = 0;
+
+        for (var i = 0; i < rows.length; i++) {
+            sum = sum + parseFloat(parseFloat(rows[i].innerHTML.substring(1, rows[i].innerHTML.length)).toFixed(2));
+        }
+
+        document.getElementById("sum").innerHTML = "Total: $" + sum;
     }
-
-    document.getElementById("sum").innerHTML = "Total: $" + sum;
-}
+});
