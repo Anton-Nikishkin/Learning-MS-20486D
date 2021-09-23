e044b276bac0b593e3a1e7d2bd313f9297ff489f
Anton Nikishkin <nikishkin.anton@gmail.com>
2021-09-23 14:41:20 +0300

    Exercise 1: Adding Entity Framework Core - Task 2: Create a class that derives from DbContext

---
+++ Module07/Cupcakes/Data/CupcakeContext.cs
@@ -0,0 +1,17 @@
+﻿using Cupcakes.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Cupcakes.Data
+{
+    public class CupcakeContext : DbContext
+    {
+        public CupcakeContext(DbContextOptions<CupcakeContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cupcake> Cupcakes { get; set; }
+        public DbSet<Bakery> Bakeries { get; set; }
+
+    }
+}
