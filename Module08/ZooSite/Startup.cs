2fe99b59b32e049fbf4d83c99846b8a9d4f87dc3
Anton Nikishkin <nikishkin.anton@gmail.com>
2021-09-23 23:40:15 +0300

    Exercise 1: Applying a Layout and Link Views to it - Task 1: Create a layout

---
+++ Module08/ZooSite/Startup.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using ZooSite.Data;
+
+namespace ZooSite
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ZooContext>(options =>
+                  options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ZooContext zooContext)
+        {
+            zooContext.Database.EnsureDeleted();
+            zooContext.Database.EnsureCreated();
+
+            app.UseRouting();
+
+            app.UseEndpoints(routes =>
+            {
+                routes.MapControllerRoute(
+                    name: "ZooRoute",
+                    pattern: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Zoo", action = "Index" },
+                    constraints: new { id = "[0-9]+" });
+            });
+        }
+    }
+}
