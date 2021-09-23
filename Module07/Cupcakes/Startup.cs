8d68f45e0c2e8c6ea701523e49a4658634e0991c
Anton Nikishkin <nikishkin.anton@gmail.com>
2021-09-23 14:48:42 +0300

    Exercise 1: Adding Entity Framework Core - Task 3: Set up Entity Framework Core to use SQLite

--- Module07/Cupcakes/Startup.cs
+++ Module07/Cupcakes/Startup.cs
@@ -1,17 +1,34 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Cupcakes.Data;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Cupcakes
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CupcakeContext>(options =>
+            options.UseSqlite("Data Source=cupcake.db"));
+
             services.AddControllersWithViews();
         }
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, CupcakeContext cupcakeContext)
         {
+            cupcakeContext.Database.EnsureDeleted();
+            cupcakeContext.Database.EnsureCreated();
+
             app.UseStaticFiles();
 
             app.UseRouting();
