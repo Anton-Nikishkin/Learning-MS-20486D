2e59a979e49b024684dc8f23902e7d062a119bf6
Anton Nikishkin <nikishkin.anton@gmail.com>
2021-09-24 03:27:27 +0300

    Exercise 1: Using gulp to Run Tasks - Task 1: Use npm to install gulp

---
+++ Module09/IceCreamCompany/Startup.cs
@@ -0,0 +1,41 @@
+﻿using IceCreamCompany.Data;
+using IceCreamCompany.Repositories;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IceCreamCompany
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IRepository, Repository>();
+
+            services.AddDbContext<IceCreamContext>(options =>
+                 options.UseSqlite("Data Source=iceCream.db"));
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IceCreamContext iceCreamContext)
+        {
+            iceCreamContext.Database.EnsureDeleted();
+            iceCreamContext.Database.EnsureCreated();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(routes =>
+            {
+                routes.MapControllerRoute(
+                    name: "IceCreamRoute",
+                    pattern: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "IceCream", action = "Index" },
+                    constraints: new { id = "[0-9]+" });
+            });
+        }
+    }
+}
