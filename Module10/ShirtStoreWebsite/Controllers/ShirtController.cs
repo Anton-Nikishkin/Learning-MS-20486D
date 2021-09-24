40234b0698e2f3206218d9dbd7639c31df9aac9f
Anton Nikishkin <nikishkin.anton@gmail.com>
2021-09-24 04:09:39 +0300

    Exercise 2: Testing a Controller using a Fake Repository - Task 3: Pass the fake repository to the constructor of a controller

--- Module10/ShirtStoreWebsite/Controllers/ShirtController.cs
+++ Module10/ShirtStoreWebsite/Controllers/ShirtController.cs
@@ -1,14 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+
 using ShirtStoreWebsite.Models;
+using ShirtStoreWebsite.Services;
 
 namespace ShirtStoreWebsite.Controllers
 {
     public class ShirtController : Controller
     {
+        private readonly IShirtRepository _repository;
+
+        public ShirtController(IShirtRepository repository)
+        {
+            _repository = repository;
+        }
 
         public IActionResult Index()
         {
