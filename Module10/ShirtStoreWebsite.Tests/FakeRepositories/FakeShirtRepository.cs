7a65fe112cfab923bb3365f86fa195c5407ac029
Anton Nikishkin <nikishkin.anton@gmail.com>
2021-09-24 04:07:14 +0300

    Exercise 2: Testing a Controller using a Fake Repository - Task 2: Implement the interface repository by using a fake repository

---
+++ Module10/ShirtStoreWebsite.Tests/FakeRepositories/FakeShirtRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+using ShirtStoreWebsite.Models;
+using ShirtStoreWebsite.Services;
+
+namespace ShirtStoreWebsite.Tests.FakeRepositories
+{
+    public class FakeShirtRepository : IShirtRepository
+    {
+        public IEnumerable<Shirt> GetShirts()
+        {
+            return new List<Shirt>()
+            {
+                new Shirt { Color = ShirtColor.Black, Size = ShirtSize.S, Price = 11F },
+                new Shirt { Color = ShirtColor.Gray, Size = ShirtSize.M, Price = 12F },
+                new Shirt { Color = ShirtColor.White, Size = ShirtSize.L, Price = 13F }
+            };
+        }
+
+        public bool AddShirt(Shirt shirt)
+        {
+            return true;
+        }
+
+        public bool RemoveShirt(int id)
+        {
+            return true;
+        }
+    }
+}
